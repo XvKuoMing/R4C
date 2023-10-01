97e9f537f5987a9e59e939d967263fa18bdb1665
Kamil <80534052+XvKuoMing@users.noreply.github.com>
2023-10-01 18:02:47 +0300

    Update urls.py

--- robots/urls.py
+++ robots/urls.py
@@ -4,5 +4,5 @@ from .views import RobotsList, RobotDetail, RobotsSummary
 urlpatterns = [
     path('api/robots', RobotsList.as_view()),
     path('api/robots/<int:id>', RobotDetail.as_view()),
-    path('api/robots_report', RobotsSummary.as_view())
+    path('api/robots/report', RobotsSummary.as_view())
 ]
